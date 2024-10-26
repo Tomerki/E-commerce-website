d189627f3c30e93c1da66d78b9b51085eb169bf0
Tomerki <Tomerh1810@gmail.com>
2024-10-26 16:58:33 +0300

    Product details reducer and action.

--- src/store.js
+++ src/store.js
@@ -1,21 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-import { productListReducers } from './reducers/productReducers';
-
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 
 const reducer = combineReducers({
-  productList: productListReducers,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
-
 
 const store = configureStore({
   reducer,
   preloadedState: initialState,
   devTools: composeWithDevTools(),
 });
-
 
 export default store;
