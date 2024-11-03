2820c60b2bab676d7a476a57ad8f7106739de03e
Tomerki <Tomerh1810@gmail.com>
2024-11-03 18:52:33 +0200

    Cart screen and add/remove to cart functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeScreen/>} exact />
             <Route path="/product/:id" element={<ProductScreen/>}/>
+            <Route path="/cart/:id?" element={<CartScreen/>}/>
           </Routes>
         </Container>
       </main>
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+  Form,
+} from "react-bootstrap";
 import Rating from "../components/Rating";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { listProductDetails } from "../actions/productActions";
 
 function ProductScreen() {
+  const [qty, setQty] = useState(1);
+  const navigate = useNavigate();
+
   const { id } = useParams();
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
@@ -16,6 +27,10 @@ function ProductScreen() {
   useEffect(() => {
     dispatch(listProductDetails(id));
   }, [dispatch, id]);
+
+  function addToCartHandler(){
+    navigate(`/cart/${id}?qty=${qty}`);
+  }
 
   return (
     <div>
@@ -75,11 +90,32 @@ function ProductScreen() {
                   </Row>
                 </ListGroup.Item>
 
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col xs="auto" className="my-1">
+                        <Form.Select
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Select>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className="w-100"
                     type="button"
-                    disabled={product.countInStock == 0}
+                    disabled={product.countInStock === 0}
                   >
                     Add to Cart
                   </Button>
