24430312264949703520121f3fececfd07e6443d
Tomerki <Tomerh1810@gmail.com>
2024-11-10 17:34:17 +0200

    user login & register screen, actions and reducres

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
+
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -17,6 +21,8 @@ function App() {
             <Route path="/" element={<HomeScreen/>} exact />
             <Route path="/product/:id" element={<ProductScreen/>}/>
             <Route path="/cart/:id?" element={<CartScreen/>}/>
+            <Route path="/login" element={<LoginScreen/>}/>
+            <Route path="/register" element={<RegisterScreen/>}/>
           </Routes>
         </Container>
       </main>
